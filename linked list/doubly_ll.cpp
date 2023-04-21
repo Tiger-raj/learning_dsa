1f028747f67b517736aef61f91f8ab5c06b91048
Tiger-raj <rajbajpai2004@gmail.com>
2023-04-21 22:38:10 +0530

    doubly linear ll done

--- linked list/doubly_ll.cpp
+++ linked list/doubly_ll.cpp
@@ -47,11 +47,107 @@ int Length(Node *p)
     return len;
 }
 
+void Insert(Node *p, int index, int x) // index for inserting starts at 0.
+{
+    Node *t = NULL;
+    if (index < 0 || index > Length(first))
+    {
+        return;
+    }
+
+    if (index == 0)
+    {
+        t = new Node;
+        t->data = x;
+        t->prev = NULL;
+        t->next = first;
+        if (first)
+        {
+            first->prev = t;
+        }
+        first = t;
+    }
+    else
+    {
+        for (int i = 0; i < index - 1; i++)
+        {
+            p = p->next;
+        }
+        t = new Node;
+        t->data = x;
+        t->next = p->next;
+        t->prev = p;
+        if (p->next)
+        {
+            p->next->prev = t;
+        }
+        p->next = t;
+    }
+}
+
+int Delete(Node *p, int index) // index for deleting starts at 1.
+{
+    Node *t = NULL;
+    int x = -1;
+
+    if (index < 1 || index > Length(first))
+    {
+        return x;
+    }
+
+    if (index == 1)
+    {
+        first = first->next;
+        if (first)
+        {
+            first->prev = NULL;
+        }
+        x = p->data;
+        delete p;
+        p = NULL;
+    }
+    else
+    {
+        for (int i = 0; i < index - 1; i++)
+        {
+            p = p->next;
+        }
+        p->prev->next = p->next;
+        if (p->next)
+        {
+            p->next->prev = p->prev;
+        }
+        x = p->data;
+        delete p;
+        p = NULL;
+    }
+    return x;
+}
+
+void Reverse(Node *p)
+{
+    Node *temp = NULL;
+    while (p)
+    {
+        temp = p->next;
+        p->next = p->prev;
+        p->prev = temp;
+        p = p->prev;
+        if (p && p->next == NULL)
+        {
+            first = p;
+        }
+    }
+}
+
 int main()
 {
     int a[] = {2, 3, 6, 5, 9, 8};
     Create(a, 6);
+    // Insert(first, 6, 1);
+    // Delete(first, 2);
     Display(first);
-    cout << Length(first);
+    Reverse(first);
+    Display(first);
     return 0;
 }
