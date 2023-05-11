ec7c413158beb339cf2c7276e32462cc9edc2bbc
Tiger-raj <rajbajpai2004@gmail.com>
2023-05-11 23:43:26 +0530

    paranthesis matching done

--- stack/adv_paranthesis_matching.cpp
+++ stack/adv_paranthesis_matching.cpp
@@ -48,20 +48,49 @@ void display(Node *top)
 
 int isBalanced(string exp, Node **top)
 {
-    int i;
+    char temp;
+    int i, d;
     for (i = 0; exp[i] != '\0'; i++)
     {
         if (exp[i] == '(' || exp[i] == '[' || exp[i] == '{')
         {
-            push(*top, exp[i]);
+            push(top, exp[i]);
+        }
+        if (exp[i] == ')' || exp[i] == ']' || exp[i] == '}')
+        {
+            if (*top == NULL)
+            {
+                return 0;
+            }
+            temp = pop(top);
+            d = exp[i] - temp;
+            cout << d << endl;
+            if ((temp < 50 && d != 1) || (temp > 50 && d != 2))
+            {
+                return 0;
+            }
         }
     }
+    if (*top != NULL)
+    {
+        return 0;
+    }
+    return 1;
 }
 
 int main()
 {
     Node *Top = NULL;
-    string str = "(a+b))-3";
-    isBalanced(str, &Top);
+    string str = "((a+b))[-3]";
+
+    if (isBalanced(str, &Top) == 1)
+    {
+        cout << "Paranthesis are balanced!\n";
+    }
+    else
+    {
+        cout << "Paranthesis are not balanced!\n";
+    }
+    display(Top);
     return 0;
 }
